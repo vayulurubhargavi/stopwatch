c0ca1c15cb8a04a3c0067aaa700733e5eb837885
vayulurubhargavi <121678183+vayulurubhargavi@users.noreply.github.com>
2022-12-30 21:14:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
+// selecting all the buttons
 const btnStart=document.querySelector('.start');
 const btnStop=document.querySelector('.stop');
 const btnReset=document.querySelector('.reset');
 
+// intialize hr,min,sec of stop watch to zero
 let hr=0;
 let min=0;
 let sec=0;
 let timer;
 
+//calltimer function--
+// first increase seconds
+// when second reaches 99 then set sec to 0 and start increasing mins
+// when min reaches 60 then set min to 0 and start increasing hrs
 function callTimer() {
     sec++;
     if (sec < 100) {
@@ -25,15 +31,17 @@ function callTimer() {
     document.getElementById("timer").innerHTML = hr + ":" + min + ":" + sec;
 }
 
+//when start button is clicked timer should start 
 btnStart.addEventListener('click',function(){
     timer=setInterval(callTimer,10)
 });
 
+//when stop button is clicked the timer should stop with its current timestamp 
 btnStop.addEventListener('click',function(){
     clearInterval(timer);
 })
 
-
+//when reset button is clicked then timer should reset to initial values 
 btnReset.addEventListener('click',function(){
     clearInterval(timer);
     hr = 0;
